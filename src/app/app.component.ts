29952b3775e8230a59701f74c94ec218f9fe9d2e
Mehmet BAKIR <mehmet.b4kir@hotmail.com>
2022-10-13 02:03:25 +0300

    kamp-angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  templateUrl: './app.component.html', /* hangi html sayfasını yöneteceği burada tanımlanıyor. */
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'northwind';
+  user: string = 'mehmet bakir';
+
+
 }
+/* Burası app.componets.html dosyasındaki dataları yönetir. */
+
